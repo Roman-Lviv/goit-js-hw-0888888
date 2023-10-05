54b5e469546388126ac0119dd7ed0777d177e8af
Roman-Lviv <kituraroman@gmail.com>
2023-10-05 18:27:23 +0300

    git add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,5 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
 // Change code below this line
 
 console.log(galleryItems);
